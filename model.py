63dd715334276f783e6326e22f44fce88b3408e5
pshop <choppin.p@gmail.com>
2017-10-31 17:59:49 +0100

    load json, instanciate as much agents that they are entries in json file, show agreeableness values/ added main function

--- model.py
+++ model.py
@@ -1,18 +1,14 @@
+import json
+
 class Agent:
 
-    def __init__(self, agent_attributes):
+    def __init__(self, **agent_attributes):
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
 
-agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona",
-"latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333,
-"longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic",
-"extraversion": 1.051833688742943,"date_of_birth": "2005-01-10",
-"agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl",
-"conscientiousness": 0.2419104411765549, "internet": "false",
-"country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        agent = Agent(**agent_attributes)
+        print(agent.agreeableness)
 
-
-agent1 = Agent(agent_attributes)
-print(agent1.neuroticism)
-print(agent1.country_name)
+main()
