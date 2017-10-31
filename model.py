0b5f452f43162ab13f264ff1f365b246ff131d99
pshop <choppin.p@gmail.com>
2017-10-31 17:23:37 +0100

    added attribute aggreeableness by default set to 0

--- model.py
+++ model.py
@@ -3,5 +3,9 @@ class Agent:
     def say_hello(self, first_name):
         return "bonjour " + first_name + "!"
 
+    def __init__(self):
+        self.agreeableness = 0
+
 agent1 = Agent()
 print(agent1.say_hello("Jean Claude"))
+print(agent1.agreeableness)
