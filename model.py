3b85f1862bfe6fa3a87d2cab379a623461725de2
pshop <choppin.p@gmail.com>
2017-10-31 17:47:45 +0100

    instanciate with a dict

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
 class Agent:
 
-    def say_hello(self, first_name):
-        return "bonjour " + first_name + "!"
+    def __init__(self, agent_attributes):
+        self.agreeableness = agent_attributes["agreeableness"]
+        print(agent_attributes.items())
 
-    def __init__(self, agreeableness):
-        self.agreeableness = agreeableness
+agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona",
+"latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333,
+"longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic",
+"extraversion": 1.051833688742943,"date_of_birth": "2005-01-10",
+"agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl",
+"conscientiousness": 0.2419104411765549, "internet": "false",
+"country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
 
-agent1 = Agent(1)
-print(agent1.say_hello("Jean Claude"))
-print(agent1.agreeableness)
+
+agent1 = Agent(agent_attributes)
