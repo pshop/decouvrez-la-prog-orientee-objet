53c92c66d4558cd71e5ae7778fe64592f750fce3
pshop <choppin.p@gmail.com>
2017-10-31 17:27:20 +0100

    agreeableness value can be now set during intanciation

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ class Agent:
     def say_hello(self, first_name):
         return "bonjour " + first_name + "!"
 
-    def __init__(self):
-        self.agreeableness = 0
+    def __init__(self, agreeableness):
+        self.agreeableness = agreeableness
 
-agent1 = Agent()
+agent1 = Agent(1)
 print(agent1.say_hello("Jean Claude"))
 print(agent1.agreeableness)
