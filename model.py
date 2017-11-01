a0dfe157de84e551819544e0960ac688a991680e
pshop <choppin.p@gmail.com>
2017-11-01 13:00:11 +0100

    added class position

--- model.py
+++ model.py
@@ -1,14 +1,29 @@
 import json
 
+###############################################################################
+
 class Agent:
 
-    def __init__(self, **agent_attributes):
+    def __init__(self, position, **agent_attributes):
+        self.position = position
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
 
+###############################################################################
+
+class Position:
+
+    def __init__(self, longitude, latitude):
+        self.longitude = longitude
+        self.latitude = latitude
+
+###############################################################################
+
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
-        agent = Agent(**agent_attributes)
-        print(agent.agreeableness)
+        latitude = agent_attributes.pop("latitude")
+        longitude = agent_attributes.pop("longitude")
+        position =  Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
 
 main()
